925e9ed268407a9b735fce01edd1055f5bd3e097
RebGov <becci.govert@gmail.com>
2020-02-09 15:25:56 -0600

    added echo and simple Rest Get works locally

--- app/main.go
+++ app/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/sirupsen/logrus"
+)
+
+var log = logrus.New()
+
+func goWithDocker(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Becci Go with Docker Container using Echo")
+}
+
+func init() {
+	log.Formatter = new(logrus.JSONFormatter)
+	log.Formatter = new(logrus.TextFormatter) //default
+	log.Level = logrus.DebugLevel
+}
 
 func main() {
 	fmt.Println("Hello Becci's World!")
+	fmt.Println("Main function :")
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	//echo Routes
+	e.GET("/main", goWithDocker)
+	e.Logger.Fatal(e.Start(":8080"))
 }
